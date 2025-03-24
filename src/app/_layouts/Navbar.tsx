5aa42f015202002600b544dc455bf6990d9c1ff4
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-03-24 22:37:08 +0700

    feat: enhance Navbar with role-based links for admin and user

--- src/app/_layouts/Navbar.tsx
+++ src/app/_layouts/Navbar.tsx
@@ -30,9 +30,31 @@ export const Navbar = async () => {
   return (
     <nav className="w-full bg-white px-4 shadow-md">
       <div className="mx-auto flex max-w-(--screen-2xl) items-center justify-between py-4">
-        <div>
-          <Link href={FrontendRoutes.HOME} className="font-semibold">
+        <div className="flex items-center gap-x-6">
+          <Link href={FrontendRoutes.HOME} className="text-lg font-semibold">
             Online Job Fair Registration
+          </Link>
+          {user?.data.role === "admin" && (
+            <Link
+              className="transition-all hover:font-medium"
+              href={FrontendRoutes.ADMIN_SESSION}
+            >
+              Dashboard
+            </Link>
+          )}
+          {user?.data.role === "user" && (
+            <Link
+              className="transition-all hover:font-medium"
+              href={FrontendRoutes.SESSION_LIST}
+            >
+              My Session
+            </Link>
+          )}
+          <Link
+            className="transition-all hover:font-medium"
+            href={FrontendRoutes.COMPANY_LIST}
+          >
+            Companies
           </Link>
         </div>
         <div className="flex items-center gap-x-4">
