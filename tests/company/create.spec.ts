ce442be00ba4f5105d787ecb388867a16c70da25
Ackaraphon Sahawiriya <ackaraphonackaraphon@gmail.com>
2025-04-20 16:51:06 +0700

    refactor: streamline company creation test and improve readability

--- tests/company/create.spec.ts
+++ tests/company/create.spec.ts
@@ -5,9 +5,7 @@ import { expect, test } from "@playwright/test";
 test.beforeEach(async ({ page }) => {
   await page.goto(withFrontendRoute(BackendRoutes.AUTH_LOGIN));
   await page.getByRole("textbox", { name: "Email" }).click();
-  await page
-    .getByRole("textbox", { name: "Email" })
-    .fill("comp01@gmail.com");
+  await page.getByRole("textbox", { name: "Email" }).fill("comp01@gmail.com");
   await page.getByRole("textbox", { name: "Password" }).click();
   await page.getByRole("textbox", { name: "Password" }).fill("12345678");
   await page.getByRole("main").getByRole("button", { name: "Sign in" }).click();
