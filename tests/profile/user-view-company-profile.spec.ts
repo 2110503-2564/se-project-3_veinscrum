5a27e0eab9d2a499a6e3e0f9a3af367d5cf2f51d
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-04-20 23:03:59 +0700

    fix: change view company to Aladin

--- tests/profile/user-view-company-profile.spec.ts
+++ tests/profile/user-view-company-profile.spec.ts
@@ -22,19 +22,24 @@ test.describe("User View Company Profile", () => {
     await expect(
       page.getByRole("heading", { name: "Online Job Fair Registration" }),
     ).toBeVisible();
-    await page.getByText("View Company").first().click();
-    await expect(page.locator("h1")).toContainText("TechNova Solutions");
-    await expect(page.locator("span")).toContainText(
-      "TechNova Solutions is a leading provider of cutting-edge software and IT services, delivering innovative digital solutions to businesses worldwide.",
-    );
-    await expect(page.locator("body")).toContainText(
-      "123 Innovation Avenue, Silicon Valley, CA 94043",
-    );
-    await expect(page.locator("body")).toContainText("http://www.google.com");
-    await expect(page.locator("body")).toContainText("0805557890");
+    await page
+      .locator("div")
+      .filter({ hasText: "Aladin" })
+      .nth(2)
+      .getByText("View Company")
+      .click();
+    await expect(page.locator("h1")).toContainText("Aladin");
+    await expect(page.locator("body")).toContainText("Tokyo summer time Saga");
+    await expect(page.locator("body")).toContainText("https://Aladin.com");
+    await expect(page.locator("body")).toContainText("0956644440");
+    await expect(
+      page.getByLabel("editable markdown").getByRole("paragraph"),
+    ).toContainText("Develop everything from Saudi environment");
     await expect(page.locator("h2")).toContainText("Job Listings");
     await expect(page.locator("body")).toContainText(
-      "No job listings available.",
+      "Junior Software Engineer",
     );
+    await expect(page.locator("body")).toContainText("Aladin");
+    await expect(page.locator("body")).toContainText("Tokyo summer time Saga");
   });
 });
