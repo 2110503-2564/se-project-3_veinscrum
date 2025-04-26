4537c201bb1ecaff0c854a95f6215600a865d308
CLOUD <6733288821@student.chula.ac.th>
2025-04-26 17:03:35 +0700

    fix: add constant in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,7 +36,6 @@ const companyRoutes = [
   FrontendRouteTargets.CHAT_SESSION,
   FrontendRouteTargets.COMPANY_CREATE,
   FrontendRouteTargets.SESSION_LIST,
-
 ];
 
 const CACHE_DURATION = 5 * 60 * 1000; // 5 minutes in milliseconds
